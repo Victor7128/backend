e0ab227a8c259d5209318b573d886840f317c110
Victor <tuestareynavictorhugo@gmail.com>
2025-05-05 22:12:30 -0500

    fix: CORS

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from login import router as auth_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],       
+    allow_methods=["*"],       
+    allow_headers=["*"],      
+    allow_credentials=False,   
+)
+
 app.include_router(auth_router)
