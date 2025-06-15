7e59d232540aad01884b6adccffae58b768b0150
Victor <tuestareynavictorhugo@gmail.com>
2025-06-14 22:39:56 -0500

    fix: filtro_histograma.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from login import router as auth_router
 from filtros.filtro_yape import router as yape_router
 from filtros.filtro_pixeles import router as pixeles_router
 from filtros.filtro_exif import router as exif_router
-from filtros.filtro_similitud import router as similitud_router
 from filtros.filtro_ruido import router as ruido_router
 from filtros.filtro_histograma import router as histograma_router
 
@@ -23,6 +22,5 @@ app.include_router(auth_router)
 app.include_router(yape_router)
 app.include_router(pixeles_router)
 app.include_router(exif_router)
-app.include_router(similitud_router)
 app.include_router(ruido_router)
 app.include_router(histograma_router)
